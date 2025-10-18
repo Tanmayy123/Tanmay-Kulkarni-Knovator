f45f0b30968a0a73eaced24cc68db0ca96a2e55e
Tanmayy123 <kulkarniworkk@gmail.com>
2025-10-18 17:12:30 +0530

    restored some changes

--- server/src/index.js
+++ server/src/index.js
@@ -10,14 +10,7 @@ const routes = require('./routes');
 const { scheduleCron } = require('./lib/scheduler');
 
 const app = express();
-
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
-
-app.use(cors({
-  origin: FRONTEND_URL,
-  credentials: true,
-}));
-
+app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(morgan(process.env.LOG_LEVEL || 'dev'));
 
@@ -45,4 +38,4 @@ bootstrap().catch((err) => {
 });
 
 
-bootstrap();
+
